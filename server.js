7ac6b4fe09598ddfdee7bcc7d1e2e4937863ccbb
NumiBlue <thekindlyblog@gmail.com>
2022-03-28 18:15:23 -0500

    handlebars

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const expbs = require(express-handlebars);
+
+app.engine('handlebars', expbs());
+app.set('view engine', 'handlebars');
 
 app.listen(8080, () => {
 console.log('Server is running at port ', 8080);
